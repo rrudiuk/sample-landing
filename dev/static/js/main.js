5f8b30e9515eb1b2ac69fa1d3bfd2451f39e2680
barik <rus.barik@gmail.com>
2017-03-02 12:52:26 +0200

    First slider added

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,2 +1,14 @@
 ;
 // Начинать писать отсюда!!!!
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  	minSlides: 1,
+  	maxSlides: 3,
+  	hideControlOnEnd: true,
+  	infiniteLoop: false,
+  	pager: false,
+  	slideWidth: 360,
+  	slideMargin: 30
+  });
+});
