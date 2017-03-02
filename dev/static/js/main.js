17a36ad9f5e3afdc9d6156c21ec4d3cb893f43dc
barik <rus.barik@gmail.com>
2017-03-02 22:17:32 +0200

    Sliders' day

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -9,6 +9,36 @@ $(document).ready(function(){
   	infiniteLoop: false,
   	pager: false,
   	slideWidth: 360,
-  	slideMargin: 30
+  	slideMargin: 30,
+  	nextText: '',
+  	prevText: '',
+  	nextSelector: '.featured-slider_next',
+  	prevSelector: '.featured-slider_prev'
   });
+});
+
+$(document).ready(function(){
+  $('.clients-bxslider').bxSlider({
+  	minSlides: 1,
+  	maxSlides: 6,
+  	hideControlOnEnd: true,
+  	infiniteLoop: false,
+  	pager: false,
+  	slideWidth: 100,
+  	slideMargin: 60,
+  	nextText: '',
+  	prevText: '',
+  	nextSelector: '.clients-slider_next',
+  	prevSelector: '.clients-slider_prev'
+  });
+
+  $(document).ready(function(){
+  $('.porfolio-bxslider').bxSlider({
+    minSlides: 1,
+    maxSlides: 4,
+    slideWidth: 400,
+    slideMargin: 0,
+    controls: false
+  });
+});
 });
